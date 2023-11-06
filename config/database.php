40a58499c1b64f4b083f7ccd51f1a95a39b0fcb4
Nathan Albuquerque <nathan.nra@outlook.com>
2023-11-06 17:50:48 -0300

    feat: copy SSL file

--- config/database.php
+++ config/database.php
@@ -87,11 +87,13 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
-            // 'collation' => 'utf8_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            // 'encrypt' => env('DB_ENCRYPT', 'yes'),
-            'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'true'),
+            'encrypt' => env('DB_ENCRYPT', 'yes'),
+            'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
+            'options' => extension_loaded('pdo_sqlsrv') ? array_filter([
+                PDO::SQLSRV_ATTR_SSL_CA => env('SQLSRV_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
     ],
